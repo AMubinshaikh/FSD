9661f5dc24bc66656094fab8503dcddc99adf0c3
AMubinshaikh <69262996+AMubinshaikh@users.noreply.github.com>
2020-10-02 22:11:31 +0530

    fsd-1 is depend on oops concept abstraction polymorphism encapsulation Inheritance Interfaces Method Overriding
    fsd 1 is completed on 2nd october in this commit has following files
    Abstraction Example
    Polymorphism_Example
    Single Inheritance
    Multi LevelI nheritance
    Method Ovveriding
    Interface Example
    
    And
    fsd 3
    Algorithm  Concept
    Completed BubbleSort with Ascending And Descending order

---
+++ fsd1-oops-concept/Abstraction_Example.cs
@@ -0,0 +1,69 @@
+/*ABSTRACTION 
+is a process in which we focus display only necessary things and hide unnecessary objects
+in other words hide implementation details from user and display only essential features
+*/
+using System;
+namespace fsd1_oops_concept
+{
+    public class AbstractionExample
+    {
+        private int _pin;
+        private string _AccountHolderName;
+        public int acc_no = 1234567;
+        
+        public int pin
+        {
+            get 
+          { 
+             return _pin;
+          }
+          set
+        {
+           _pin = value;
+        }
+          
+        }
+
+        public string AccountHolderName
+
+        {
+             get
+            {
+                return _AccountHolderName;
+            }
+            set
+            {
+              _AccountHolderName = value;
+            }
+           
+        }
+
+        public void display() // display because its necessary and public
+        {
+
+            Console.WriteLine("Customer Details name ="+ AccountHolderName+  "And account number = " + acc_no );
+        }
+
+        private void Displaypin() // private doesnt show its unnecessary
+        {
+
+            Console.WriteLine("pin of customer");
+        }
+
+
+    }
+
+    public class Abstraction_Example
+    {
+        public static void Main(string[] args)
+    {
+
+        AbstractionExample a = new AbstractionExample();
+        a.AccountHolderName = "zubinshaikh";
+        a.display(); // accessible and no error
+       // a.displayPin(); //  inaccesible because it is private
+    }
+    }
+}
+
+    
---
+++ fsd1-oops-concept/Polymorphism_Example.cs
@@ -0,0 +1,46 @@
+/*
+// polymorphism is a process in which we can use single object for many purpose in this program 
+// Add() is a  function which is overloading means using Add function two tmes but there is no ambiguity because we are
+// using different number of parameter or we can use different type of parameters of same function name
+*/
+using System;
+namespace fsd1_oops_concept
+{
+        public class AddingNumberPoly
+    {
+         public static int Add(int num1, int num2) // with two integer parameter
+         {
+            return num1+num2; 
+         }
+         public int Add(int num1, int num2 , int num3) // same name function overloading but three parameter
+         {
+            return num1+num2+num3;
+         }
+        }
+
+    public class Polymorphism_Example
+    {
+        public static void Main(string[] args)
+         {
+             AddingNumberPoly a  = new AddingNumberPoly();  // "a" is a instance of a class
+
+             // calling Add Function by using
+             //class name because Add is an Static type function 
+
+             int n1,n2,n3;
+             Console.WriteLine("Enter Two Numbers");
+             n1 = Convert.ToInt32(Console.ReadLine());
+             n2 = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("The sum of two Number is = " + AddingNumberPoly.Add(n1,n2)); 
+
+             
+              //calling using object this is not static member function
+             
+             Console.WriteLine("Enter three Numbers");
+             n1 = Convert.ToInt32(Console.ReadLine());
+             n2 = Convert.ToInt32(Console.ReadLine());
+             n3 = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("The sum of two Number is = " + a.Add(n1,n2,n3)); 
+         }
+         }
+}
