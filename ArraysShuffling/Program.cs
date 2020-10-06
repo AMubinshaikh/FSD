767a63b64b3f9dc6766d925fa91ebb7a75ea1300
AMubinshaikh <69262996+AMubinshaikh@users.noreply.github.com>
2020-10-06 20:03:23 +0530

    FSD-5 is completed and ready to merge with  Develop branch
    FSD-5  is based on shuffling arrays
    with simple flow
    please check.

---
+++ ArraysShuffling/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ArraysShuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // program1
+             ShufflingProgram1 shufflingProgram1 = new ShufflingProgram1();
+             shufflingProgram1.AcceptArray();
+             shufflingProgram1.DisplayShuffledElements();
+            //----------------------------------------------------------
+            // program2
+
+            ShufflingProgram2 shufflingProgram2 = new ShufflingProgram2();
+            shufflingProgram2.AcceptArray(); 
+            shufflingProgram2.Shuffle();
+        }
+    }
+}
