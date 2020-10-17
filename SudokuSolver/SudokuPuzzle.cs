c735a3d40b29cbe57959c9c967de10825d351744
AMubinshaikh <69262996+AMubinshaikh@users.noreply.github.com>
2020-10-17 16:54:34 +0530

    Changes are done
    according to PR comments indentation and naming conventions are changed.

--- SudokuSolver/SudokuPuzzle.cs
+++ SudokuSolver/SudokuPuzzle.cs
@@ -1,105 +1,67 @@
 using System;
- 
-namespace SudokuSolver
-{
-  class SudokuPuzzle
-  {
-    public static bool SolveSudoku(int[,] puzzle, int row, int col)
-    {
-	   if (row < 9 && col < 9)
-	   {
-		   if (puzzle[row, col] != 0)
-		   {
-			if ((col + 1) < 9) 
-			{
-			return SolveSudoku(puzzle, row, col + 1);
-			}
-			  else 
-			  {
-                if ((row + 1) < 9) 
-			    {
-				  return SolveSudoku(puzzle, row + 1, 0);
-			    }
-			  else
-			  {
-                 return true;
-			  } 
-		      }
-	       }
-		     else
-		   {
-			  for (int arrayLoop = 0; arrayLoop < 9; ++arrayLoop)
-			  {
-				  if (IsAvailable(puzzle, row, col, arrayLoop + 1))
-				  {
-					 puzzle[row, col] = arrayLoop + 1;
 
-					 if ((col + 1) < 9)
-					 {
-						if (SolveSudoku(puzzle, row, col + 1))
-						{
-                          return true;
-						} 
-						else 
-						{
-							puzzle[row, col] = 0;
-						}
-					}
-					 else 
-					 {
-						if ((row + 1) < 9)
-					    {
-						if (SolveSudoku(puzzle, row + 1, 0)) 
-						{
+namespace SudokuSolver {
+	class SudokuPuzzle {
+		public static bool SolveSudoku (int[, ] puzzle, int row, int col) {
+			if (row < 9 && col < 9) {
+				if (puzzle[row, col] != 0) {
+					if ((col + 1) < 9) {
+						return SolveSudoku (puzzle, row, col + 1);
+					} else {
+						if ((row + 1) < 9) {
+							return SolveSudoku (puzzle, row + 1, 0);
+						} else {
 							return true;
 						}
-						else 
-						{
-							puzzle[row, col] = 0;
+					}
+				} else {
+					for (int number = 0; number < 9; ++number) {
+						if (IsAvailable (puzzle, row, col, number + 1)) {
+							puzzle[row, col] = number + 1;
+
+							if ((col + 1) < 9) {
+								if (SolveSudoku (puzzle, row, col + 1)) {
+									return true;
+								} else {
+									puzzle[row, col] = 0;
+								}
+							} else {
+								if ((row + 1) < 9) {
+									if (SolveSudoku (puzzle, row + 1, 0)) {
+										return true;
+									} else {
+										puzzle[row, col] = 0;
+									}
+								} else {
+									return true;
+								}
+							}
 						}
-					    }
-					
-					 else 
-					 {
-						return true;
-					 }
-					 }
-				  }
-			  }
-		   }   
+					}
+				}
 
-		    return false;
-	    }
-	    else 
-	    {
-		   return true;
-	    }
-    }
+				return false;
+			} else {
+				return true;
+			}
+		}
+		private static bool IsAvailable (int[, ] puzzle, int row, int col, int num) {
+			int rowStart = (row / 3) * 3;
+			int colStart = (col / 3) * 3;
 
-     private static bool IsAvailable(int[,] puzzle, int row, int col, int num)
-     {
-	     int rowStart = (row / 3) * 3;
-	     int colStart = (col / 3) * 3;
+			for (int number = 0; number < 9; ++number) {
+				if (puzzle[row, number] == num) {
+					return false;
+				}
+				if (puzzle[number, col] == num) {
+					return false;
+				}
+				if (puzzle[rowStart + (number % 3), colStart + (number / 3)] == num) {
+					return false;
+				}
+			}
+			return true;
+		}
 
-	     for (int arrayLoop = 0; arrayLoop < 9; ++arrayLoop)
-	     {
-		     if (puzzle[row, arrayLoop] == num) 
-			 {
-				 return false;
-			 }
-		     if (puzzle[arrayLoop, col] == num) 
-			 {
-				 return false;
-			 }
-		     if (puzzle[rowStart + (arrayLoop % 3), colStart + (arrayLoop / 3)] == num) 
-			 {
-				 return false;
-			 }
-	     }
-
-	        return true;
-      }
-   
-  }
+	}
 }
-
