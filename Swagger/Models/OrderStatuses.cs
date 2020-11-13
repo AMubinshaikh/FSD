25cc61c1a3b02fb5aaf9846f988613f658918835
AMubinshaikh <69262996+AMubinshaikh@users.noreply.github.com>
2020-11-13 10:51:38 +0530

    fsd 24 Swagger
    Implement swagger in web API.

---
+++ Swagger/Models/OrderStatuses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Swagger_2.Models
+{
+    public partial class OrderStatuses
+    {
+        public OrderStatuses()
+        {
+            Orders = new HashSet<Orders>();
+        }
+
+        public byte OrderStatusId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Orders> Orders { get; set; }
+    }
+}
