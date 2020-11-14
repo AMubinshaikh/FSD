8cfd1b56173afd06f30ebc493c1ff30bfffe7e94
AMubinshaikh <69262996+AMubinshaikh@users.noreply.github.com>
2020-11-14 17:06:20 +0530

    Delete OrderItemNoteResource.cs

--- Swagger/Controllers/Resources/OrderItemNoteResource.cs
+++
@@ -1,9 +0,0 @@
-
-namespace Swagger_2.Controllers.Resources {
-    public class OrderItemNoteResource {
-        public int NoteId { get; set; }
-        public int OrderId { get; set; }
-        public int ProductId { get; set; }
-        public string Note { get; set; }
-    }
-} 
